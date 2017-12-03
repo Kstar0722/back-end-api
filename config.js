076c460ade4a3ce5ee3d62985f99f6b300ee754e
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-12-02 19:40:00 -0500

    improved send email notification

--- config.js
+++ config.js
@@ -2,11 +2,19 @@
 
 let config = {
   development: {
-    token_secret: 'secret'
+    token_secret: 'secret',
+    domain: 'localhost:3000',
+    dashboard_url: 'dashboard',
+    s3_region: 'us-west-2',
+    s3_bucket: 'iifym-blueprints',
   },
   production: {
-    token_secret: process.env.TOKEN_SECRET
+    token_secret: process.env.TOKEN_SECRET,
+    domain: process.env.DOMAIN || 'iifym-dashboard.s3-website-us-west-2.amazonaws.com', // iifym.com
+    dashboard_url: process.env.DASHBOARD || 'dashboard', // member-dashboard
+    s3_region: process.env.S3_REGION || 'us-west-2',
+    s3_bucket: process.env.S3_BUCKET || 'iifym-blueprints',
   }
-}
+};
 
 module.exports = config[process.env.NODE_ENV || 'development'];
